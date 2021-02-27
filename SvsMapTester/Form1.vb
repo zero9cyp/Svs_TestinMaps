ec662ef92b3185bb643f69af21fee961647b0583
SPR_1 <svsnik71@gmail.com>
2021-02-27 16:29:45 +0200

    Add project files.

---
+++ SvsMapTester/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.IO
+Imports System.Reflection
+Imports System.Security.Permissions
+Imports System.Text
+<PermissionSet(SecurityAction.Demand, Name:="FullTrust")>
+<System.Runtime.InteropServices.ComVisible(True)>
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.wbmap.ObjectForScripting = Me
+
+        Dim onlyPositions As Double(,) = New Double(2, 1) {{42.13557, -0.40806}, {42.13684, -0.40884}, {42.13716, -0.40729}}
+        Dim positonAndTitles As String(,) = New String(2, 2) {{"42.13557", "-0.40806", "marker0"}, {"42.13684", "-0.40884", "marker1"}, {"42.13716", "-0.40729", "marker2"}}
+        Dim positonTitlesAndIcons As String(,) = New String(2, 3) {{"42.13557", "-0.40806", "marker0", "truck_red.png"}, {"42.13684", "-0.40884", "marker1", "truck_red.png"}, {"42.13716", "-0.40729", "marker2", "truck_red.png"}}
+
+        'Dim gmh As GoogleMapHelper = New GoogleMapHelper(wbmap, onlyPositions)
+        'Dim gmh As GoogleMapHelper = New GoogleMapHelper(wbmap, positonAndTitles)
+        Dim gmh As GoogleMapHelper = New GoogleMapHelper(wbmap, positonTitlesAndIcons)
+        gmh.loadMap()
+    End Sub
+
+    '############################### CALLING JAVASCRIPT METHODS ##############################
+    'This methods call methods written in googlemap_template.html
+    Private Sub callMapJavascript(sender As Object, e As EventArgs) Handles Button1.Click
+        wbmap.Document.InvokeScript("showJavascriptHelloWorld")
+    End Sub
+
+    Private Sub callMapJavascriptWithArguments(sender As Object, e As EventArgs) Handles Button2.Click
+        wbmap.Document.InvokeScript("focusMarkerFromIdx", New String() {2})
+    End Sub
+    '#########################################################################################
+
+    '############################### METHODS CALLED FROM JAVASCRIPT ##########################
+    'This methods are called by the javascript defined in googlemap_template.html when some events are triggered
+    Public Sub getMarkerDataFromJavascript(title As String, idx As String)
+        MsgBox("Title: " & title & " idx: " & idx)
+    End Sub
+
+    Public Sub showVbHelloWorld()
+        MsgBox("Hello world in WF from HTML")
+    End Sub
+End Class
---
+++ SvsMapTester/GoogleMapHelper.vb
@@ -0,0 +1,133 @@
+﻿Imports System.IO
+Imports System.Reflection
+Imports System.Text
+
+Public Class GoogleMapHelper
+
+    ' 1- googlemap_template.html must be copied in the main project folder
+    ' 2- add the file into the Visual Studio Solution Explorer (add existing file)
+    ' 3- set the properties of the file to: 
+    '                                   Build Action -> Embedded Resource
+    '                                   Custom Tool Namespace -> write the name of the project
+
+    Private Const ICON_FOLDER As String = "marker_icons/" 'images must be stored in a folder inside  Debug/Release folder
+    Private Const MAP_TEMPLATE As String = "SvsMapTester.googlemap_template.html"
+    Private Const TEXT_TO_REPLACE_MARKER_DATA As String = "[[MARKER_DATA]]"
+    Private Const TMP_NAME As String = "tmp_map.html"
+
+
+    Private mWebBrowser As WebBrowser
+
+    'MARKER POSITIONS 
+    Private mPositions As Double(,) 'lat, lon
+    ' marker data allows different formats to include lat,long and optionally title and icon:
+    ' op1: mMarkerData = New String(N-1, 1) {{lat1, lon1}, {lat2, lon2}, {latN, lonN}} 
+    ' op2: mMarkerData = New String(N-1, 2) {{lat1, lon1,'title1'}, {lat2, lon2,'title2'}, {latN, lonN, 'titleN'}} 
+    ' op3: mMarkerData = New String(N-1, 3) {{lat1, lon1,'title1','image1.png'}, {lat2, lon2,'title2','image2.png'}, {latN, lonN, 'titleN','imageN.png'}} 
+    Private mMarkerData As String(,) = Nothing
+
+
+    Public Sub New(ByRef wb As WebBrowser, pos As Double(,))
+        mWebBrowser = wb
+        mPositions = pos
+        mMarkerData = getMarkerDataFromPositions(pos)
+    End Sub
+
+    Public Sub New(ByRef wb As WebBrowser, md As String(,))
+        mWebBrowser = wb
+        mMarkerData = md
+    End Sub
+
+    Public Sub loadMap()
+        mWebBrowser.Navigate(getMapTemplate())
+    End Sub
+
+    Private Function getMapTemplate() As String
+
+        If mMarkerData Is Nothing Or mMarkerData.GetLength(1) > 4 Then
+            MessageBox.Show("Marker data has not the proper size. It must have 2, 3 o 4 columns")
+            Return Nothing
+        End If
+
+        Dim htmlTemplate As New StringBuilder()
+        Dim tmpFolder As String = Environment.GetEnvironmentVariable("TEMP")
+        Dim dataSize As Integer = mMarkerData.GetLength(1) 'number of columns
+        Dim mMarkerDataAsText As String = String.Empty
+        Dim myresourcePath As String = My.Resources.ResourceManager.BaseName
+        Dim myresourcefullPath As String = Path.GetFullPath(My.Resources.ResourceManager.BaseName)
+        Dim localPath = myresourcefullPath.Replace(myresourcePath, "").Replace("\", "/") & ICON_FOLDER
+
+        htmlTemplate.AppendLine(getStringFromResources(MAP_TEMPLATE))
+        mMarkerDataAsText = "["
+
+        For i As Integer = 0 To mMarkerData.GetLength(0) - 1
+            If i <> 0 Then
+                mMarkerDataAsText += ","
+            End If
+            If dataSize = 2 Then 'lat,lon
+                mMarkerDataAsText += "[" & mMarkerData(i, 0) & "," + mMarkerData(i, 1) & "]"
+            ElseIf dataSize = 3 Then 'lat,lon and title
+                mMarkerDataAsText += "[" & mMarkerData(i, 0) & "," + mMarkerData(i, 1) & ",'" & mMarkerData(i, 2) & "']"
+            ElseIf dataSize = 4 Then 'lat,lon,title and image
+                mMarkerDataAsText += "[" & mMarkerData(i, 0) & "," + mMarkerData(i, 1) & ",'" & mMarkerData(i, 2) & "','" & localPath & mMarkerData(i, 3) & "']" 'Ojo a las comillas simples en las columnas 3 y 4 
+            End If
+        Next
+
+        mMarkerDataAsText += "]"
+        htmlTemplate.Replace(TEXT_TO_REPLACE_MARKER_DATA, mMarkerDataAsText)
+
+        Dim tmpHtmlMapFile As String = (tmpFolder & Convert.ToString("\")) + TMP_NAME
+        Dim existsMapFile As Boolean = False
+        Try
+            existsMapFile = createTxtFile(tmpHtmlMapFile, htmlTemplate)
+        Catch ex As Exception
+            MessageBox.Show("Error writing temporal file", "Writing Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
+
+        If existsMapFile Then
+            Return tmpHtmlMapFile
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Private Function getMarkerDataFromPositions(pos As Double(,)) As String(,)
+        Dim md As String(,) = New String(pos.GetLength(0) - 1, 1) {}
+        For i As Integer = 0 To pos.GetLength(0) - 1
+            md(i, 0) = pos(i, 0).ToString("g", New System.Globalization.CultureInfo("en-US"))
+            md(i, 1) = pos(i, 1).ToString("g", New System.Globalization.CultureInfo("en-US"))
+        Next
+        Return md
+    End Function
+
+    Private Function getStringFromResources(resourceName As String) As String
+        Dim assem As Assembly = Me.[GetType]().Assembly
+
+        Using stream As Stream = assem.GetManifestResourceStream(resourceName)
+            If stream IsNot Nothing Then
+                Try
+                    Using reader As New StreamReader(stream)
+                        Return reader.ReadToEnd()
+                    End Using
+                Catch e As Exception
+                    Throw New Exception((Convert.ToString("Error de acceso al Recurso '") & resourceName) + "'" & vbCr & vbLf + e.ToString())
+                End Try
+            End If
+
+        End Using
+    End Function
+
+    Private Function createTxtFile(mFile As String, content As StringBuilder) As Boolean
+        Dim mPath As String = Path.GetDirectoryName(mFile)
+        If Not Directory.Exists(mPath) Then
+            Directory.CreateDirectory(mPath)
+        End If
+        If File.Exists(mFile) Then
+            File.Delete(mFile)
+        End If
+        Dim sw As StreamWriter = File.CreateText(mFile)
+        sw.Write(content.ToString())
+        sw.Close()
+        Return True
+    End Function
+End Class
